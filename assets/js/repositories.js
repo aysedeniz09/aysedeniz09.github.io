d59645683c3ce657719d6b06382c8d69faed2943
Ayse Lokmanoglu <alokman@bu.edu>
2024-11-12 15:15:30 -0500

    added repositories

---
+++ assets/js/repositories.js
@@ -0,0 +1,72 @@
+// GitHub username
+const username = 'aysedeniz09'; // replace with your GitHub username
+// Optional: GitHub token for higher rate limit and private repos
+const token = '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';
+
+// API URL
+const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+async function fetchRepositories() {
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                Authorization: `token ${token}` // use token for higher rate limits
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const repos = await response.json();
+        const repoContainer = document.getElementById('repo-container');
+
+        if (!repoContainer) {
+            console.error('Container element not found');
+            return;
+        }
+
+        repoContainer.innerHTML = ''; // Clear any existing content
+
+        repos.forEach(repo => {
+            // Language color object
+            const languageColors = {
+                "JavaScript": "#f1e05a",
+                "Python": "#3572A5",
+                "HTML": "#e34c26",
+                "CSS": "#563d7c",
+                "Java": "#b07219",
+                "Jupyter Notebook": "#DA5B0B",
+                "R": "#198CE7",
+                // Add more languages and their colors here
+            };
+
+            const languageColor = languageColors[repo.language] || '#cccccc';
+
+            // Create repository card
+            const repoCard = document.createElement('div');
+            repoCard.classList.add('repository-card');
+
+            // Set inner HTML for each repository card
+            repoCard.innerHTML = `
+                <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
+                <div class="repo-info">
+                    ${repo.language ? `<span class="repo-language" style="color: ${languageColor};">&#9679;</span> ${repo.language}` : ''}
+                </div>
+                <div class="badges">
+                    <span class="badge">⭐ ${repo.stargazers_count}</span>
+                    <span class="badge">🍴 ${repo.forks_count}</span>
+                    <span class="badge">🚨 ${repo.open_issues_count}</span>
+                </div>
+            `;
+
+            // Append the repository card to the container
+            repoContainer.appendChild(repoCard);
+        });
+    } catch (error) {
+        console.error('Error fetching repositories:', error);
+    }
+}
+
+// Initialize fetching of repositories
+fetchRepositories();
